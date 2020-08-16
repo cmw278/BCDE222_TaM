40f76a87b261738e04a77d82987266599a33b9bf
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-17 11:35:03 +1200

    Add project files.

---
+++ Theseus and the Minotaur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Theseus_and_the_Minotaur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
